844683b9f8d2cebe85957507d6976a80f50d6962
Anakaren Lozano <aklozzg@gmail.com>
2024-05-06 17:53:52 -0700

    readying for deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Base_Datos",
+  base: "/Base_Datos/index.html",
   plugins: [react()],
 })
